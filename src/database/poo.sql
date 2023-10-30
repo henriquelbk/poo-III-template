4bfb23b317d0612d2a358914cb39612bcaf39ba0
henriquelbk <loubackhenrique123@gmail.com>
2023-10-30 20:41:39 -0300

    indo

--- src/database/poo.sql
+++ src/database/poo.sql
@@ -1,17 +1,15 @@
--- Active: 1675168195024@@127.0.0.1@3306
+-- Active: 1698703920158@@127.0.0.1@3306
 CREATE TABLE users (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL,
-    created_at TEXT NOT NULL
+    password TEXT NOT NULL
 );
 
 CREATE TABLE accounts (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     owner_id TEXT NOT NULL,
     balance REAL DEFAULT (0) NOT NULL,
-    created_at TEXT NOT NULL,
     FOREIGN KEY (owner_id) REFERENCES users (id)
         ON UPDATE CASCADE
         ON DELETE CASCADE
@@ -26,3 +24,7 @@ INSERT INTO accounts (id, owner_id)
 VALUES
 	('a001', 'u001'),
 	('a002', 'u002');
+
+DROP TABLE users;
+
+DROP TABLE accounts;
--- src/types.ts
+++ src/types.ts
@@ -26,3 +26,40 @@ export interface AccountDBPost {
     id: string,
     owner_id: string
 }
+
+// Entra nenhum parametro e sai number
+
+abstract class Produto {
+    constructor(
+        public id: string,
+        protected preco: number
+    ) {}
+
+    public getPreco(): number {
+        return this.preco
+    }
+}
+
+class ProdutoFisico extends Produto {
+    constructor(id: string, preco: number) {
+        super(id, preco)
+    }
+
+    public getPreco(): number {
+        return this.preco * 1.05 // custo de estoque
+    }
+}
+
+class ProdutoVirtual extends Produto {
+    constructor(id: string, preco: number) {
+        super(id, preco)
+    }
+
+    public getPreco(): number {
+        return this.preco * 0.95 // desconto pela facilidade do online
+    }
+
+}
+
+const cafe = new ProdutoFisico('cafe', 10)
+cafe.getPreco()
